c90d395adce8f95987caeceffab7691d574bcc62
Tukeba Martins Pedro <tukebamartinspedrolury@gmail.com>
2026-02-20 08:08:44 +0000

    fix: overflow-hidden no header para border radius funcionar

--- apps/web/src/components/layout/Navbar.tsx
+++ apps/web/src/components/layout/Navbar.tsx
@@ -35,7 +35,7 @@ export function Navbar() {
 
   return (
     <div className="w-full px-4 sm:px-6 lg:px-8 pt-4 sm:pt-6">
-      <header className="relative w-full max-w-7xl mx-auto bg-black flex flex-col rounded-[30px] sm:rounded-[40px]">
+      <header className="relative w-full max-w-7xl mx-auto bg-black flex flex-col rounded-[30px] sm:rounded-[40px] overflow-hidden">
 
         {/* NAVBAR */}
         <nav className="w-full z-50 bg-black/40 backdrop-blur-lg h-20 relative ">
